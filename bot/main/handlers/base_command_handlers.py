afde21e9af5d8af024872612ccd42e312f69696e
Nikolaev3Artem <artemnikolayev607@gmail.com>
2024-11-21 10:36:15 +0200

    Added bot messages from admin

--- bot/main/handlers/base_command_handlers.py
+++ bot/main/handlers/base_command_handlers.py
@@ -6,6 +6,7 @@ from backend.services.user_service import UserService
 from bot.main.bot_instance import bot, dp
 from bot.main.constants.enums import StartMenuEnum
 from bot.main.keyboards.blockchain_survey import start_keyboard
+from bot.repositories.messages_repo import message_repository
 
 @dp.message_handler(commands=["start"])
 async def send_welcome(message: types.Message):
@@ -30,57 +31,32 @@ async def send_welcome(message: types.Message):
             )
             await user_repository.create_user(user)
             await user_repository.update_invited_by(message["from"]["id"], invited_by=invited_by)
-
-    await message.answer(
-        """
-Добро пожаловать на Olegobot, этот бот предназначен для дропов. Пожалуйста введите адрес своего кошелька,
-поставьте 0 если у вас нет его в этой сети, то нужно заполнить хотя бы 1 адрес.
-        """,
-        reply_markup=start_keyboard,
-    )
+    start_message = await message_repository.get_start_message()
+    await message.answer(start_message.message, reply_markup=start_keyboard)
 
 
 @dp.callback_query_handler(lambda c: c.data == StartMenuEnum.ACCOUNT)
 async def send_acc_info(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
-    refferal_link = await UserService.get_refferal_link(user_id=user_id)
-    refferals_count = await UserService.get_user_refferals_count(user_id=user_id)
-    user_points = await UserService.get_user_points(user_id=user_id)
+
+    account_message = await message_repository.get_account_message()
+    context = {
+        "refferal_link": await UserService.get_refferal_link(user_id=user_id),
+        "refferals_count": await UserService.get_user_refferals_count(user_id=user_id),
+        "user_points": await UserService.get_user_points(user_id=user_id),
+    }
+    formatted_message = account_message.message.format(**context)
 
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(
         callback_query.from_user.id,
-        f"""
-
-Твоя реферальная ссылка {refferal_link}
-
-Рефералов: {refferals_count}
-
-Баллов: {user_points} Olegopoints
-        """,
-    )
+        formatted_message)
 
 
 @dp.callback_query_handler(lambda c: c.data == StartMenuEnum.HELP)
 async def send_help(callback_query: types.CallbackQuery):
+    help_message = await message_repository.get_help_message()
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(
-        callback_query.from_user.id,
-        """
-Всё очень просто. Вы заполняете ваши криптоадреса и получаете за это поинты.\n
-Приглашаете своих друзей заполнить их данные - и ваши друзья получат х2 от
-обычного количества поинтов за анкету, а вы получите бонус Y поинтов за каждого.\n
-Зачем нужны поинты?\n
-Представьте, что какой-то новый токен хочет обратить на себя внимание холдеров токена $brett.\n
-Они обращаются к нам и говорят - мы готовы дать аирдропом 5% нашего токена всем активным холдерам $brett.\n
-Мы проверяем этот новый токен на отсутствие скама (высокий налог на продажу, минтабл, 90% сапплая у команды и т.д.)\n
-Если всё ок - мы выбираем из нашей базы анкет всех, кто держит сейчас
-$brett хотя бы на 5 долларов, и рассылаем им предложение получить аирдроп.\n
-Всем, кто соглашается, мы присылаем аирдроп пропорционально их поинтам.\n
-То есть, например, у нас есть 1.000.000 токенов на аирдроп и 100 человек согласилось его получить.\n
-У них всех в сумме 20.000 поинтов - значит, мы раздадим токенов из расчёта 50 токенов за 1
-поинт. Если у вас 100 поинтов - получите 5000 токенов, если 200 - то получите 10000 токенов.\n\n
-Поинты можно заработать не только заполнением анкет или приглашением друзей - следите за нашими рассылками в боте!\n
-Иногда мы будем просто раздавать поинты в честь каких-либо событий (например,\t
-10.000 пользователей или 25.000 пользователей бота - чем не повод всех порадовать).""",
+        callback_query.from_user.id, help_message.message
     )
--- bot/main/handlers/blockchain_handler.py
+++ bot/main/handlers/blockchain_handler.py
@@ -2,11 +2,11 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 from backend.constants.enums import BlockchainEnum
-from backend.services.blockchain_service import BlockchainService
 from backend.services.user_service import UserService
 from bot.main.bot_instance import bot, dp
 from bot.main.keyboards.command_button import command_keyboard
 from core.settings import settings
+from bot.repositories.messages_repo import message_repository
 
 user_state = {}
 
@@ -19,16 +19,21 @@ async def select_blockchain(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == "FinishSurvey")
 async def handle_callback_button_end(callback_query: types.CallbackQuery, state: FSMContext):
-    refferal_link = f"https://t.me/{settings.BOT_NICKNAME}?start={callback_query['from']['id']}"
+    user_id = callback_query['from']['id']
+    refferal_link = f"https://t.me/{settings.BOT_NICKNAME}?start={user_id}"
     await UserService.update_refferral_link_link(
         user_id=callback_query["from"]["id"],
         refferral_link=refferal_link,
     )
+    survey_completed = await message_repository.get_survey_completed_message()
+    context = {
+        "refferal_link": refferal_link,
+    }
+    formatted_message = survey_completed.message.format(**context)
+
     await bot.send_message(
         callback_query.from_user.id,
-        f"""Спасибо! Свяжемся когда ваши активы заинтересуют нас.\n
-Вот твоя реферальная ссылка {refferal_link}\n
-Делись с друзьями, за это ты будешь получать Олегобаллы, которые ты сможкешь использовать для...""",
+        formatted_message,
         reply_markup=command_keyboard,
     )
     await state.finish()
