05f434713f3b189aff2ae0c6cb019dc1307688ab
Denys <denis06699@gmail.com>
2024-11-22 21:12:05 +0200

    Validation has been added for different Ethereum networks.

--- backend/services/blockchain_service.py
+++ backend/services/blockchain_service.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 
 from backend.models import Addresses
@@ -5,7 +7,7 @@ from backend.repositories.blockchain_repo import blockchain_repository
 from backend.schemas.address import AddressCreate, AddressUpdate
 from backend.services.user_service import UserService
 from bot_backend.settings import ETHERSCAN_HOST, ETHERSCAN_TOKEN, TRONSCAN_HOST
-import re
+
 
 class BlockchainService:
 
@@ -25,25 +27,26 @@ class BlockchainService:
 
     @staticmethod
     async def validate_etereum_address(address: str) -> dict:
-        if not re.fullmatch(r'0x[a-fA-F0-9]{40}', address):
+        if not re.fullmatch(r"0x[a-fA-F0-9]{40}", address):
             return False
-        responce = requests.get(
-            f"{ETHERSCAN_HOST}?chainid=1&module=account&action=tokentx&address={address}&apikey={ETHERSCAN_TOKEN}"
-        )
-        if responce.status_code == 200:
-            return True
-        elif responce.status_code == 0:
-            return False
+        chainids = [1, 8453, 56, 137]
+        for item in chainids:
+            responce = requests.get(
+                f"{ETHERSCAN_HOST}?chainid={item}&module=account&action=tokentx&address={address}&apikey={ETHERSCAN_TOKEN}"
+            )
+            if responce.status_code == 200 and responce.json()["status"] == "1" and responce.json()["message"] == "OK":
+                return True
+        return False
 
     @staticmethod
     async def validate_solana_address(address: str) -> bool:
-        if not re.fullmatch(r'[1-9A-HJ-NP-Za-km-z]{32,44}', address):
+        if not re.fullmatch(r"[1-9A-HJ-NP-Za-km-z]{32,44}", address):
             return False
         return True
 
     @staticmethod
     async def validate_tron_address(address: str) -> bool:
-        if not re.fullmatch(r'T[A-Za-z1-9]{33}', address):
+        if not re.fullmatch(r"T[A-Za-z1-9]{33}", address):
             return False
         responce = requests.get(f"{TRONSCAN_HOST}?address={address}&asset_type=0")
         responce_data = responce.json()
